8c6f8b0b772862d10489cc2725cf39767cbc8b96
AlexLinux <adegroat917@gmail.com>
2014-05-16 23:28:23 -0600

    Refactor pages tests

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -2,26 +2,21 @@ require 'spec_helper'
 
 describe "Pages" do
 
+	subject { page }
 
 	describe "Home page" do
-		it "should have content 'Sample App'" do
-			visit root_path
-			expect(page).to have_content('Sample App')
-		end
+		before { visit root_path }
+		it { should have_content('Sample App') }
 	end
 
 	describe "Help page" do
-		it "should have content 'Help'" do
-			visit help_path
-			expect(page).to have_content('Help')
-		end
+		before { visit help_path }
+		it{ should have_content('Help') }
 	end
 
 	describe "About page" do
-		it "should have content 'About'" do
-			visit about_path
-			expect(page).to have_content('About')
-		end
+		before {visit about_path}
+		it{should have_content('About')}
 	end
 
 end
