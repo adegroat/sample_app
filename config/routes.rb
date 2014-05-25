282f913245b2b12d3784e946a4746734fb684404
AlexLinux <adegroat917@gmail.com>
2014-05-25 13:18:31 -0600

    Initial setup for signin and signout

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 SampleApp::Application.routes.draw do
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root 'pages#home'
   match '/home', to: 'pages#home', via: 'get'
   match '/help', to: 'pages#help', via: 'get'
   match '/about', to: 'pages#about', via: 'get'
   match '/contact', to: 'pages#contact', via: 'get'
   match '/signup', to: 'users#new', via: 'get'
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
