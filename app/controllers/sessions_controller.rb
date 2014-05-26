094e5c0ca9b6897dd2fbda553b00fecca0d406a1
AlexLinux <adegroat917@gmail.com>
2014-05-26 00:26:21 -0600

    Initial steps for signing i. have invalid signin working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,20 @@
 class SessionsController < ApplicationController
 
 	def new
+		@email = ""
+		@password = ""
 	end
 
 	def create
+		user = User.find_by(email: params[:session][:email])
+
+		if user && user.authenticate(params[:session][:password])
+			redirect_to(user)
+		else
+			flash.now[:error] = "Invalid username/password."
+			render 'new'
+		end
+	
 	end
 
 	def destroy
